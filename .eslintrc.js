1ba9eedf7d3bb2af7aff321af70b8bcd82302da9
Joe Campo <jcampo@gmail.com>
2022-01-11 09:58:24 -0500

    Update eslint config to use Vue 3 recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:vue/recommended",
+    "plugin:vue/vue3-recommended",
     "prettier/vue",
     "plugin:prettier/recommended"
   ],
