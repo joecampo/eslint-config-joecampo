b80032a8f2e53a2c547ddc341410606ac79d98b4
Joe Campo <jcampo@gmail.com>
2022-01-11 10:42:49 -0500

    Update parser to @babel/eslint-parser and disable requireConfigFile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,26 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended",
-    "prettier",
-    "plugin:prettier/recommended"
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier', 'plugin:prettier/recommended'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         printWidth: 120,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: "all"
-      }
-    ]
+        trailingComma: 'all',
+      },
+    ],
   },
   globals: {
-    'axios': 'readonly',
-    '_': 'readonly',
-    '$': 'readonly',
-    'Vue': 'readonly',
-    'moment': 'readonly'
+    axios: 'readonly',
+    _: 'readonly',
+    $: 'readonly',
+    Vue: 'readonly',
+    moment: 'readonly',
   },
   parserOptions: {
-    parser: "babel-eslint",
-    ecmaVersion: 2018
-  }
+    requireConfigFile: false,
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2018,
+  },
 };
